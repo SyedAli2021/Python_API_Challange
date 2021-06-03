9226c5e51cdd5cd25119270bdb7148058e13f222
SyedAli2021 <77982758+SyedAli2021@users.noreply.github.com>
2021-06-03 17:53:02 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "df875ea84c5945bcd0265ea3ffa5d910"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBPq_7XvSAN1XWyFdoFLCn3EMFXJJYeSdg"
+g_key = ""
